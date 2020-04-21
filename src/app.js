6ef06d17f76052feadfa41ecf6c2f40c146adf59
Student-Java <energoslav@gmail.com>
2020-04-21 11:16:46 -0300

    feat: add cors and helmet

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+const cors = require('cors');
+const helmet = require('helmet');
 const wordsRouter = require('./resources/words/word.router');
 const statisticsRouter = require('./resources/statistics/statistics.router');
 const settingsRouter = require('./resources/settings/settings.router');
@@ -10,6 +12,8 @@ const errorHandler = require('./errors/errorHandler');
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
