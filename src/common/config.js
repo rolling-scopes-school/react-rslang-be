2bcad777a4187ce277cf01279852b595e23cd287
Student-Java <energoslav@gmail.com>
2020-06-08 21:16:56 -0300

    feat: change limit of optional fields

--- src/common/config.js
+++ src/common/config.js
@@ -11,8 +11,8 @@ module.exports = {
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
-  MAX_SYMBOLS_PER_OBJECT: 1500,
-  MAX_OPTIONAL_PROPERTIES: 30,
+  MAX_SYMBOLS_PER_OBJECT: 5000,
+  MAX_OPTIONAL_PROPERTIES: 100,
   LOGS_DIR: path.join(__dirname, '../../logs'),
   JWT_EXPIRE_TIME: '4h'
 };
