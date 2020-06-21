34cd67b9574c9bdbfafd86f686d34f7694c5c995
Student-Java <energoslav@gmail.com>
2020-06-21 13:38:51 -0300

    feat: change aggregatedWord endpoint filtering approach

--- src/resources/aggregatedWords/aggregatedWord.service.js
+++ src/resources/aggregatedWords/aggregatedWord.service.js
@@ -1,7 +1,7 @@
 const wordRepo = require('./aggregatedWord.db.repository');
 
-const getAll = async (userId, group, perPage, filter, onlyUserWords) =>
-  wordRepo.getAll(userId, group, perPage, filter, onlyUserWords);
+const getAll = async (userId, group, perPage, filter) =>
+  wordRepo.getAll(userId, group, perPage, filter);
 
 const get = async (wordId, userId) => wordRepo.get(wordId, userId);
 
