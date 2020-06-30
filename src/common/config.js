a9e9054fd1d5ae1cca439fd7fae37bc3b056d168
Student-Java <energoslav@gmail.com>
2020-06-30 19:01:35 -0300

    refactor: extend optional length limit up to 10000 symbols

--- src/common/config.js
+++ src/common/config.js
@@ -11,7 +11,7 @@ module.exports = {
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
-  MAX_SYMBOLS_PER_OBJECT: 5000,
+  MAX_SYMBOLS_PER_OBJECT: 10000,
   MAX_OPTIONAL_PROPERTIES: 100,
   LOGS_DIR: path.join(__dirname, '../../logs'),
   JWT_EXPIRE_TIME: '4h'
