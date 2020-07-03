1e710809e02dbb5fe346a571ff26af320b622642
Student-Java <energoslav@gmail.com>
2020-07-03 13:51:12 -0300

    refactor: add query parameter to get word without assets

--- src/resources/words/word.service.js
+++ src/resources/words/word.service.js
@@ -8,36 +8,38 @@ const getAll = async conditions => wordRepo.getAll(conditions);
 const getQuantity = async (group, wordsPerExampleSentenceLTE) =>
   wordRepo.getQuantity(group, wordsPerExampleSentenceLTE);
 
-const get = async wordId => {
+const get = async (wordId, noAssets) => {
   const word = await wordRepo.get(wordId);
 
-  word.image = await fs.readFile(
-    path.join(__dirname, `../../../${word.image}`),
-    {
-      encoding: 'base64'
-    }
-  );
+  if (!noAssets) {
+    word.image = await fs.readFile(
+      path.join(__dirname, `../../../${word.image}`),
+      {
+        encoding: 'base64'
+      }
+    );
 
-  word.audio = await fs.readFile(
-    path.join(__dirname, `../../../${word.audio}`),
-    {
-      encoding: 'base64'
-    }
-  );
+    word.audio = await fs.readFile(
+      path.join(__dirname, `../../../${word.audio}`),
+      {
+        encoding: 'base64'
+      }
+    );
 
-  word.audioMeaning = await fs.readFile(
-    path.join(__dirname, `../../../${word.audioMeaning}`),
-    {
-      encoding: 'base64'
-    }
-  );
+    word.audioMeaning = await fs.readFile(
+      path.join(__dirname, `../../../${word.audioMeaning}`),
+      {
+        encoding: 'base64'
+      }
+    );
 
-  word.audioExample = await fs.readFile(
-    path.join(__dirname, `../../../${word.audioExample}`),
-    {
-      encoding: 'base64'
-    }
-  );
+    word.audioExample = await fs.readFile(
+      path.join(__dirname, `../../../${word.audioExample}`),
+      {
+        encoding: 'base64'
+      }
+    );
+  }
 
   return word;
 };
